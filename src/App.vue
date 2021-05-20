1cef071f103580c6b4baa238d4fc6e9cfb7229a8
iru0225 <irsyadhafizh2@gmail.com>
2021-05-20 21:48:51 +0700

    update App component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <InfiniteScroll/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InfiniteScroll from './components/infinite-scroll/infinite-scroll.component';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InfiniteScroll
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  
 </style>
